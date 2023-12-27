4743479951a21f16d371b7029e96c51477163009
Franco L. Deluca <frandeluca22@gmail.com>
2023-12-27 12:38:58 -0300

    Finished anecdotes

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+
+const Button = ({onClick, text}) => {
+  return (
+    <>
+      <button onClick={onClick}>{text}</button>
+    </>
+  );
+};
+
+const Anecdote = ({text}) => {
+    return <div>{text}</div>
+}
+
+const VoteCounter = ({counter}) => {
+  return <div><p>This anecdote has {counter} votes.</p></div>
+}
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Array(8).fill(0));
+  const [maxValue, setMaxValue] = useState(0);
+
+  const handleNextClick = () => {
+    setSelected((Math.floor(Math.random() * anecdotes.length)));
+  };
+
+  const handleVote = () => {
+    const updatedArray = [...votes]
+    updatedArray[selected] += 1;
+    setVotes(updatedArray);    
+    setNewMaxValue();
+  } 
+
+  const setNewMaxValue = () => {
+    let maxValue = -1;
+    let position = -1;
+    for (let index = 0; index < votes.length; index++) {
+      if(votes[index] > maxValue) {
+        maxValue = votes[index];
+        position = index;
+      }
+    }
+    setMaxValue(position);
+  }
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <Anecdote text={anecdotes[selected]} />
+      <VoteCounter counter={votes[selected]} />
+      <Button text='Vote' onClick={handleVote}/>
+      <Button text='Next anecdote' onClick={handleNextClick} />
+      <h1>Anecdote with the most votes</h1>
+      <Anecdote text={anecdotes[maxValue]} />
+    </div>
+  );
+};
+
+export default App;
